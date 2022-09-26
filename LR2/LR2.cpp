15d6e19cd515c2fa2e2f3f72c1a4c9e9a3dffc6d
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-09-26 12:33:38 +0300

    LR2

---
+++ LR2/LR2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string rle(string code)
+{
+	int count = 0;
+	string encoded = "";
+	for (int i = 0; i < code.length() - 1; i++) {
+		count = 1;
+		while (code[i] == code[i + 1]) {
+			count++;
+			i++;
+		}
+		if (count > 1)
+			encoded += to_string(count) + code[i];
+		else
+			encoded += code[i];
+	}
+	if (code[code.length() - 1] != encoded[encoded.length() - 1])
+		encoded += code[code.length() - 1];
+	return encoded;
+}
+
+int main()
+{
+	cout << rle("AAAVVVCSSSLL");
+
+}
